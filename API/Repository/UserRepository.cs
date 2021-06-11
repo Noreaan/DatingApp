b131d5d805c2ca308cf706ee85bc5fde20150fb4
Michiel <michvdd@hotmail.com>
2021-06-11 14:31:06 +0200

    Repo

---
+++ API/Repository/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using API.Data;
+using API.Entities;
+using API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Task<AppUser> GetUserByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppUser> GetUserByUserNameAsync(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
